1200850f3a8547a30a6651469e2255e040b7fd49
Julia Gardholm <juliamejl@hotmail.com>
2018-04-13 14:55:49 +0200

    new files to sidemenu

---
+++ src/components/SettingsScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Icon, Button, Container, Header, Content, Left, Title, Right, Body } from 'native-base';
+
+class SettingsScreen extends Component { 
+    render() {
+        return (
+            <Container>
+
+                <Header>
+                    <Left>
+                        <Icon name="ios-menu" onPress={() =>
+                        this.props.navigation.navigate('DrawerOpen')}/>
+                    </Left>
+                    <Body>
+                            <Title>Settings</Title>
+                        </Body>
+                    <Right />
+                </Header>
+                 <Content contentContainerStyle={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                    <Text>Settings Screen</Text>
+                </Content>
+            </Container>
+
+        );
+    }
+}
+export default SettingsScreen;
