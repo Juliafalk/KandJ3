7a6a9a963288fea378771795d69531cce23280be
jennyliindgren <jenny.lindgren@hotmail.com>
2018-04-27 16:42:49 +0200

    styling footer in map, step 1

--- src/components/Map.js
+++ src/components/Map.js
@@ -19,6 +19,7 @@ import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplet
 import Geocoder from 'react-native-geocoding';
 import firebase from 'firebase';
 import haversine from 'haversine';
+import { MyInput } from './common';
 //import SummaryPage from './SummaryPage';
 
 const { width, height } = Dimensions.get('window');
@@ -328,11 +329,11 @@ class Map extends Component {
 
     if (!this.state.startRunning){
       return(
-        <View style={{backgroundColor: '#5c688c'}}>
+        <View>
           <View style={createRouteContainerStyle}>
             <View style={actualDistanceStyle}>
-              <Text style={{ fontSize: 12}}>This Route:</Text>
-              <Text>{actualDistance.toFixed(2)} km</Text>
+              <Text style={{ fontSize: 12, color: 'white'}}>This Route:</Text>
+              <Text style={{ color: 'white'}}>{actualDistance.toFixed(2)} km</Text>
             </View>
             <View style={inputContainerStyle}>
               <TextInput
@@ -345,10 +346,10 @@ class Map extends Component {
                   wantedDistance: userInput}),
                   this.changeDistance(userInput)}}
               />
-              <Text>km</Text>
             </View>
             <Button
               info
+              full
               style={createRouteButtonStyle}
               disabled ={createRoute}
               onPress={() => {this.routeGenerator(wantedDistance), 
@@ -357,7 +358,7 @@ class Map extends Component {
             </Button>
           </View>
           <Button
-          block
+          full
           success
           disabled={startButton}
           style={startButtonStyle}
@@ -410,7 +411,6 @@ class Map extends Component {
                   [
                     {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
                     {text: 'OK', onPress: () => {this.SummaryPage(), this.setState({ totalDuration: TOTAL_DURATION }), this.toDatabase()}
-            
                     },
                   ],
                   { cancelable: false }
@@ -448,12 +448,8 @@ class Map extends Component {
   //JL 11/4: the render function adds markers at all waypoints and draws the route inbetween them
   render() {
     const {
-      createRouteContainerStyle,
-      createRouteButtonStyle,
       inputContainerStyle,
       distanceContainer,
-      textInputStyle,
-      actualDistanceStyle,
       startButtonStyle,
       distanceTravelledStyle,
       timeContainer,
@@ -473,7 +469,7 @@ class Map extends Component {
     } = this.state;
 
     return (
-      <View>
+      <View style={styles.mapPageContainer}>
         <MapView
           provider={"google"}
           showsUserLocation={true}
@@ -486,8 +482,7 @@ class Map extends Component {
           style={styles.mapStyle}
           ref={c => this.mapView = c}
          >
-          <View
-          style={{ height: '37%' }}>
+          <View>
             {this.chooseStartpoint()}
           </View>
           <MapView.Marker 
@@ -538,7 +533,7 @@ class Map extends Component {
           )}
 
         </MapView>
-        {this.startRunning()}
+          {this.startRunning()}
       </View>
     );
   }
@@ -555,15 +550,18 @@ class Map extends Component {
 //****STYLING*****//
 //Obs, styling for clock is in the bottom under const options / JF (18/4)
 const styles = {
+  mapPageContainer: {
+    height: '96%',
+    backgroundColor: '#5c688c'
+  },
   mapStyle: {
-    height: '67%'
+    height: '80%'
   },
   createRouteContainerStyle: {
-    height: '22%',
     marginTop: 10,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',  
+    //alignItems: 'center',
   },
   distanceContainer: {
     width: '35%',
@@ -591,19 +589,22 @@ const styles = {
     alignItems: 'center'
   },
   textInputStyle: {
-    width: '40%',
+    width: '60%',
+    height: 40,
     paddingTop: 5,
     paddingBottom: 5,
+    fontSize: 20,
+    color: 'white',
     textAlign: 'center',
     marginRight: 5,
     borderWidth: 1,
-    borderRadius: 5
+    borderColor: 'white'
   },
   actualDistanceStyle: {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    marginRight: 15
+    marginRight: 15,
   },
   createRouteButtonStyle: {
     width: '30%',
@@ -611,9 +612,7 @@ const styles = {
     alignItems: 'center',
   },
   startButtonStyle: {
-    margin: 10,
-    //marginLeft: -1,
-    //marginRight: -1
+    marginTop: 10
   },
   pauseDoneContainer: {
     flexDirection: 'row',
@@ -626,7 +625,7 @@ const styles = {
     marginLeft: 5
   },
   divideSection: {
-    height: '30%',
+    //height: '30%',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -636,7 +635,7 @@ const styles = {
   }
 }
 
-//This is styling for the clock / JF (18/4)
+//This is styling for the timer / JF (18/4)
 const options = {
   container: {
     padding: 5,
@@ -648,7 +647,7 @@ const options = {
     marginLeft: 2,
   }
 };
-
+ 
 /****************HERE STARTS A NEW CLASS FOR SUMMARYPAGE*****************/
 class TheSummary extends React.Component {
   static navigationOptions = {
@@ -664,8 +663,8 @@ class TheSummary extends React.Component {
         <View style={{  height: '100%', backgroundColor: '#5c688c' }}>
         <View style={{ marginLeft: 15, marginTop: 10 }}>
         <Icon name='close' 
-        onPress={() => {this.props.navigation.navigate('Home')}} 
-        style={{ fontSize: 50, color: 'red' }}
+          onPress={() => {this.props.navigation.navigate('Home')}} 
+          style={{ fontSize: 50, color: 'red' }}
         />
         </View>
         <View style={styles.divideSection}>
